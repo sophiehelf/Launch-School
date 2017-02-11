4db1c905c100c9c18bd0e39b02bd363763975340
Sophie Helf <sophie.helf@gmail.com>
2017-02-10 23:42:02 -0800

    first commit

---
+++ intro_to_programming/basics.rb
@@ -0,0 +1,36 @@
+# 1
+"Sophie" + " Helf"
+
+#2
+thousands = 1293 % 1000
+hundreds = 3489 % 1000 / 100
+tens = 9593 % 1000 % 100  / 10
+ones = 4958 % 1000 % 100 % 10
+
+# 3 
+movies = {:harold_and_maude=>1973, :akira=>1988, :lost_in_translation=>2003, :fantastic_planet:1971}
+puts movies[:harold_and_maude]
+puts movies[:akira]
+puts movies[:lost_in_translation]
+puts movies[:fantastic_planet]
+
+#4
+movie_array = [1971, 1988, 2003, 1971]
+puts movie_array[0]
+puts movie_array[1]
+puts movie_array[2]
+puts movie_array[3]
+
+#5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#6
+puts 1.1**2
+puts 3.5**2
+puts 9.5**2
+
+#7 
+#Expected a hash bracket and instead got a parenthese
