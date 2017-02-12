d3bfc71074f4ee1059664389985d91b432af192d
Sophie Helf <sophie.helf@gmail.com>
2017-02-11 20:43:27 -0800

    Variables

---
+++ intro_to_programming/Variables/name.rb
@@ -0,0 +1,26 @@
+#1 
+
+puts "What's your name?"
+name = gets.chomp
+puts "What's your name?"
+puts "Hi " + name + ", how are you today?"
+
+#3 
+10.times do |x|
+	puts name
+end
+
+#4 
+
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts first_name + last_name
+
+#5 
+#puts out 3
+#error: x is not defined as it is out of scope
+
+#6
+# The variable shoes is out of scope
